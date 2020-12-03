ee5a91da432f8a7a1e2b93fcf0cc5269bf679569
jtaylor617 <73145984+jtaylor617@users.noreply.github.com>
2020-12-02 19:26:43 -0500

    Create httpserver.py

---
+++ httpserver.py
@@ -0,0 +1,18 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+PORT = 40025
+
+class helloHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write("helloooo".encode())
+
+def main():
+    server = HTTPServer(('', PORT), helloHandler)
+    print('Server running on port %s' % PORT )
+    server.serve_forever()
+
+if __name__ == '__main__':
+    main()
